c1aab1b66328a069586fa860694bcfe5c1870411
Virecus <virecus@gmail.com>
2026-06-05 11:28:15 +0300

    oto repo added.

--- app/src/main/java/com/nuvio/tv/NuvioApplication.kt
+++ app/src/main/java/com/nuvio/tv/NuvioApplication.kt
@@ -17,6 +17,7 @@ import coil3.request.allowRgb565
 import coil3.bitmapFactoryMaxParallelism
 
 import okio.Path.Companion.toOkioPath
+import com.nuvio.tv.core.plugin.DefaultRepoBootstrapService
 import com.nuvio.tv.core.runtime.PluginRuntimeHooks
 import com.nuvio.tv.core.sync.StartupSyncService
 import com.nuvio.tv.core.sync.androidtv.AndroidTvChannelSyncService
@@ -33,6 +34,7 @@ class NuvioApplication : Application(), SingletonImageLoader.Factory {
 
     @Inject lateinit var startupSyncService: StartupSyncService
     @Inject lateinit var androidTvChannelSyncService: AndroidTvChannelSyncService
+    @Inject lateinit var defaultRepoBootstrapService: DefaultRepoBootstrapService
 
     companion object {
         /**
@@ -63,6 +65,7 @@ class NuvioApplication : Application(), SingletonImageLoader.Factory {
         super.onCreate()
         PluginRuntimeHooks.onApplicationCreate(this)
         androidTvChannelSyncService.start()
+        defaultRepoBootstrapService.bootstrap()
         // Load locale synchronously so it's available before Activity.attachBaseContext.
         // SharedPreferences reads are fast (cached in memory after first access).
         val tag = getSharedPreferences("app_locale", Context.MODE_PRIVATE)
